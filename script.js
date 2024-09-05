eaaab2a90ee6b79b50656f02a282c0177d1b4b83
SofiaRydell <sofia__rydell@hotmail.com>
2024-09-05 23:22:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function loadArticle(section) {
+    let articles = {
+        'gravid': "Under graviditeten kan du utföra lågintensiva träningspass som simning, promenader och gravid-yoga. Undvik tunga lyft och intensiva övningar som belastar bäckenet.",
+        'efter': "Efter förlossningen bör du fokusera på återhämtning och successivt börja med lätta övningar som knipövningar och promenader. Kontakta en fysioterapeut för råd kring vad som passar dig bäst."
+    };
+    
+    alert(articles[section]);
+}
